be1157864dfdd9a46d86c88a0ca26dde7100b7fa
Albritt <devin.albritton@gmail.com>
2023-07-07 21:25:23 -0400

    Adding script file

---
+++ sdvx.py
@@ -0,0 +1,18 @@
+# Importing modules
+from tkinter import *
+import rotatescreen
+
+def screen_rotation(input):
+	screen = rotatescreen.get_primary_display()
+	if input == "horizontal"
+		screen.set_landscape()
+	elif input == "vertical"
+		screen.set_portrait()
+		
+		
+mainmenu = Tk()
+mainmenu.geometry(100*100)
+mainmenu.title("SDVX Setup/Teardown")
+
+
+
